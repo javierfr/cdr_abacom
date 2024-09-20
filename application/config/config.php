d1f135992bcebee79654ebaa744f96062afc69e5
Francisco Javier Ferruzca Rojas <javier_fr@outlook.com>
2024-09-20 12:52:48 -0600

    conf database baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/AbaCalls/';
-// $config['base_url'] = 'https://www.abacom.mx/AbaCalls/';
+// $config['base_url'] = 'http://localhost/AbaCalls/';
+$config['base_url'] = 'https://cdr.abacom.mx';
 
 /*
 |--------------------------------------------------------------------------
